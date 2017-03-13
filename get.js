721ace8dfaf1dd6b9f7020b8e8795a7dd50b8b44
KEIBigdataResearch <jinhyeongkim@kei.re.kr>
2017-03-13 16:41:43 +0900

    js file upload

---
+++ get.js
@@ -0,0 +1,58 @@
+var google = require('googleapis');
+
+function auth (callback) {
+  google.auth.getApplicationDefault(function (err, authClient) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    // The createScopedRequired method returns true when running on GAE or a
+    // local developer machine. In that case, the desired scopes must be passed
+    // in manually. When the code is  running in GCE or GAE Flexible, the scopes
+    // are pulled from the GCE metadata server.
+    // See https://cloud.google.com/compute/docs/authentication for more
+    // information.
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+      // Scopes can be specified either as an array or as a single,
+      // space-delimited string.
+      authClient = authClient.createScoped([
+        'https://www.googleapis.com/auth/devstorage.read_only',
+        'https://www.googleapis.com/auth/prediction'
+      ]);
+    }
+    callback(null, authClient);
+  });
+}
+
+function get (callback) {
+  auth(function (err, authClient) {
+
+    if (err) {
+      return callback(err);
+    }
+
+    var trainedmodels = google.prediction({
+      version: 'v1.6',
+      auth: authClient
+    }).trainedmodels;
+
+    trainedmodels.get({
+      project: 'test-project-160901',
+      id: "random-regression"         
+    }, function (err, res){
+      if(err) console.log(err);
+      console.log(res);
+    });
+
+  });
+}
+
+// Run the examples
+exports.main = function (phrase, cb) {
+  get(cb || console.log);
+};
+
+if (module === require.main) {
+  exports.main(process.argv.slice(2).shift());
+}
